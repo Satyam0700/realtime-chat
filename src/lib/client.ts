0e79a92327ca5748999af0071a8f83ebfe693fd8
Satyam 0700 <gamerzhind199@gmail.com>
2026-01-03 21:18:29 +0530

    domain-change

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,7 +2,7 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>(process.env.SERVER_URL || '').api
+export const client = treaty<App>("https://shh-mode.vercel.app").api
 
 // import { treaty } from '@elysiajs/eden'
 // import { app } from '../app/api/[[...slugs]]/route'
