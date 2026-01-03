f801d431def058db27db07fbb1896bcdadc5757e
Satyam Kumar <gamerzhind199@gmail.com>
2026-01-03 21:04:44 +0530

    Clean up redis.ts by removing unused code
    Removed commented-out Redis initialization code.

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,9 +1,4 @@
-// import { Redis } from '@upstash/redis'
 
-// export const redis = new Redis({
-//   url: 'https://natural-bobcat-7225.upstash.io',
-//   token: 'ARw5AAImcDI0OWFlYjk5MGVkNjU0NzBkYmViMGVkZGIxZjQ0NmZiOHAyNzIyNQ',
-// })
 
 import { Redis } from '@upstash/redis'
 
