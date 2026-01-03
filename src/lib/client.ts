04bea60a2d5b4a8b1d1dc7c2a105de2727b700f5
Satyam 0700 <gamerzhind199@gmail.com>
2026-01-03 21:15:53 +0530

    api-changes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,7 +2,7 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>('localhost:3000').api
+export const client = treaty<App>(process.env.SERVER_URL || '').api
 
 // import { treaty } from '@elysiajs/eden'
 // import { app } from '../app/api/[[...slugs]]/route'
