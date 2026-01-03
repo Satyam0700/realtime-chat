7bbe8674886603c421664ada165b39d8f0d3a4bf
Satyam 0700 <gamerzhind199@gmail.com>
2026-01-03 21:37:59 +0530

    env-changes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,7 +2,7 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // Get API URL from environment or use default
-const apiUrl = process.env.NEXT_PUBLIC_API_URL! || 'http://localhost:3000'
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 
 // .api to enter /api prefix
-export const client = treaty<App>("https://shh-mode.vercel.app").api
+export const client = treaty<App>(apiUrl).api
